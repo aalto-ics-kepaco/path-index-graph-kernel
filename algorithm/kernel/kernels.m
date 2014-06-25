6741b6814a8cb005c146b5fd0a80301368b971a3
cle-ment <cle-ment@gmx.de>
2014-06-25 16:12:18 +0300

    updated kernels

--- algorithm/kernel/kernels.m
+++ algorithm/kernel/kernels.m
@@ -11,15 +11,17 @@ function [ K ] = kernels(input_file_features, output_file_kernels)
     tic
     
     % euclidean distance
-    disp('Computing kernels using euclidean distance.');
-    Q=repmat(dot(Phi,Phi,2),1,size(Phi,1));
-    D=sqrt(Q+Q'-2*Phi*Phi');
-    
-    scaledD = (D-min(D(:))) ./ (max(D(:)-min(D(:))));
-    
-    K = full(1- scaledD);
-    
+%    disp('Computing kernels using euclidean distance.');
+%    Q=repmat(dot(Phi,Phi,2),1,size(Phi,1));
+%    D=sqrt(Q+Q'-2*Phi*Phi');
+ 
+K = full(normalize(Phi * Phi'))
+   
     toc
+    
+%    scaledD = (D-min(D(:))) ./ (max(D(:)-min(D(:))));
+    
+%    K = full(1- scaledD);
 
     dlmwrite(output_file_kernels, K, ' ');
 
